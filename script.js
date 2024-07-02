a5b74e3672d3c73f125f93712bc5b55a6278f6ad
wenqiangzhang <wenqiangzhang@pharmaoryx.com>
2024-07-02 21:38:50 +0800

    feat:新增页面-张文强

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
   let lastScrollTop = 0;
   const navbar = document.getElementById("navbar");
-  const logoImage = document.getElementById("logo-image");
-
   window.addEventListener("scroll", function () {
+    // navbar.style.backgroundColor = "rgba(0,0,0)";
     let scrollTop = window.scrollY;
-
-    if (scrollTop > lastScrollTop) {
-      // 向下滚动时，让导航栏自然离开页面
-      navbar.style.position = "absolute";
-      logoImage.style.position = "absolute";
+    if (scrollTop !== 0) {
+      if (scrollTop > lastScrollTop) {
+        // 向下滚动时，让导航栏自然离开页面
+        navbar.style.position = "absolute";
+      } else {
+        // 向上滚动时，将导航栏固定在顶部
+        navbar.style.position = "fixed";
+        navbar.style.top = "0";
+        navbar.style.backgroundColor = "rgba(0,0,0)";
+      }
     } else {
-      // 向上滚动时，将导航栏固定在顶部
-      navbar.style.position = "fixed";
-      navbar.style.top = "0";
-      navbar.style.backgroundColor = "rgba(0,0,0)";
-
-      logoImage.style.position = "fixed";
-      logoImage.style.top = "0";
+      navbar.style.backgroundColor = "rgba(0,0,0,0)";
     }
+
     lastScrollTop = scrollTop;
   });
 });

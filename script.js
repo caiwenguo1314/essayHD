3c8649dcc692ba983e08758fcf604b4223b9fd65
wenqiangzhang <wenqiangzhang@pharmaoryx.com>
2024-07-10 23:38:07 +0800

    feat:新增网页-张文强

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
 document.addEventListener("DOMContentLoaded", function () {
+  function searchInsert(nums, target) {
+    if (nums[nums.length / 2]) {
+      if (target <= nums[nums.length / 2]) {
+      }
+    } else {
+    }
+  }
+
+  function getInsert(nums, target) {
+    if (target < nums[0]) {
+      return 0;
+    }
+    console.log("Math.log2(4)", Math.log2(4));
+    console.log("Math.log2(9)", Math.log2(9));
+    for (let i = 0; i < nums.length - 1; i++) {
+      if (target === nums[i]) {
+        return i;
+      } else if (nums[i] === target - 1 && nums[i + 1] > target) {
+        return i + 1;
+      }
+    }
+
+    if (nums[nums.length - 1] === target) {
+      return nums.length - 1;
+    } else if (nums[nums.length - 1] < target) {
+      return nums.length;
+    }
+  }
+
+  console.log("searchInsert([1,3,5,6]),5", searchInsert([1, 3, 5, 6]), 5);
+
+  console.log("getInsert([1,3,5,6],5)", getInsert([1, 3, 5, 6], 5));
+  console.log("getInsert([1,3,5,6],4)", getInsert([1, 3, 5, 6], 4));
+  console.log("getInsert([1,3,5,6],2)", getInsert([1, 3, 5, 6], 2));
+  console.log("getInsert([1,3,5,6],6)", getInsert([1, 3, 5, 6], 6));
+  console.log("getInsert([1,3,5,6],7)", getInsert([1, 3, 5, 6], 7));
+  console.log("getInsert([1,3,5,6],0)", getInsert([1, 3, 5, 6], 0));
+
   document
     .getElementById("scroll-link1")
     .addEventListener("click", function () {

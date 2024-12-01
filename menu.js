1bf090aaf4a2ea4979455a100febf56cdff24b69
caihong <328668100@qq.com>
2024-12-01 21:04:49 +0800

    20241201修改

---
+++ menu.js
@@ -0,0 +1,9 @@
+// menu.js
+function toggleMenu() {
+  const menu = document.querySelector('.hamburger-menu');
+  if (menu.style.display === 'block') {
+    menu.style.display = 'none';
+  } else {
+    menu.style.display = 'block';
+  }
+}
